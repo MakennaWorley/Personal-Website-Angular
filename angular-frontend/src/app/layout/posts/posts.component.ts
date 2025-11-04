cd0005de47e108a90fef3d62c76f903eefa30e67
Makenna Worley <39921273+MakennaWorley@users.noreply.github.com>
2025-11-04 10:13:48 -0700

    Feat Nest Backend

--- angular-frontend/src/app/layout/posts/posts.component.ts
+++ angular-frontend/src/app/layout/posts/posts.component.ts
@@ -1,31 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
-import { PostMetadata, PostService } from './post.service';
 
 @Component({
 	selector: 'app-posts',
 	imports: [CommonModule, MaterialModule],
 	templateUrl: './posts.component.html',
 	styleUrl: './posts.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true
 })
 export class PostsComponent {
-	posts: PostMetadata[] = [];
-	contentMap: { [filename: string]: string } = {};
-
-	constructor(private postService: PostService) {}
-
-	ngOnInit(): void {
-		this.postService.getPosts().subscribe((posts) => (this.posts = posts));
-		console.log(this.posts);
-	}
-
-	loadContent(post: PostMetadata): void {
-		if (!this.contentMap[post.file]) {
-			this.postService.getPostContent(post.file).subscribe((content) => {
-				this.contentMap[post.file] = content;
-			});
-		}
-	}
+	readonly panelOpenState = signal(false);
 }
