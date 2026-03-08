3a17b8c30c510aa35750b23a62b50d16b4eb9eed
Makenna Worley <39921273+MakennaWorley@users.noreply.github.com>
2026-03-07 20:41:59 -0700

    Prettier Formatting

--- angular-frontend/src/app/layout/posts/posts.component.spec.ts
+++ angular-frontend/src/app/layout/posts/posts.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostsComponent } from './posts.component';
 
 describe('PostsComponent', () => {
-  let component: PostsComponent;
-  let fixture: ComponentFixture<PostsComponent>;
+	let component: PostsComponent;
+	let fixture: ComponentFixture<PostsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PostsComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [PostsComponent]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(PostsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(PostsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
